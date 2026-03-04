75bfcbc423bf4003b883fa1bc0e511ebf6ac283e
wobsoriano <sorianorobertc@gmail.com>
2026-03-03 20:43:50 -0800

    chore: add svelte eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,14 @@
 import eslint from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
 import tseslint from "typescript-eslint";
+import svelteConfig from "./packages/@visual-json/svelte/svelte.config.js";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  ...svelte.configs["flat/base"],
   {
-    ignores: ["**/dist/**", "**/.next/**", "**/node_modules/**"],
+    ignores: ["**/dist/**", "**/.next/**", "**/node_modules/**", "**/.svelte-kit/**"],
   },
   {
     rules: {
@@ -22,6 +25,21 @@ export default tseslint.config(
         console: "readonly",
         process: "readonly",
       },
+    },
+  },
+  {
+    files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+    languageOptions: {
+      parserOptions: {
+        extraFileExtensions: [".svelte"],
+        parser: tseslint.parser,
+        svelteConfig,
+      },
+    },
+    rules: {
+      "no-undef": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
     },
   },
 );
